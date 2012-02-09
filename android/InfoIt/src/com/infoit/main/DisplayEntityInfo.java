1804a6f9ce7aaa4323b278c0ed0f68768f370392
Christian Deonier <cdeonier@gmail.com>
2012-02-08 19:51:17 -0800

    Cleaned prototype

--- android/InfoIt/src/com/infoit/main/DisplayEntityInfo.java
+++ android/InfoIt/src/com/infoit/main/DisplayEntityInfo.java
@@ -1,10 +1,14 @@
 package com.infoit.main;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
-import com.infoit.util.ShellUtil;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class DisplayEntityInfo extends FragmentActivity {
 
@@ -18,7 +22,60 @@ public class DisplayEntityInfo extends FragmentActivity {
     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     setContentView(R.layout.display_entity_info);
     
-    ShellUtil.initializeShellForActivity(this);
+    ImageView openMapButton = (ImageView) findViewById(R.id.open_map_button);
+    ImageView getDirectionsButton = (ImageView) findViewById(R.id.get_directions_button);
+    ImageView contactAgentButton = (ImageView) findViewById(R.id.contact_agent_button);
+    ImageView agentDetailsButton = (ImageView) findViewById(R.id.agent_details_button);
+    LinearLayout photosButton = (LinearLayout) findViewById(R.id.photos_button);
+    
+    openMapButton.setOnClickListener(new OnClickListener(){
+      @Override
+      public void onClick(View arg0) {
+        String mapUrl = "http://maps.google.com/maps?f=q&source=s_q&hl=en&geocode=&q=855+Spruance+Lane,+Foster+City,+CA&aq=0&oq=855+spruance&sll=37.568168,-122.312573&sspn=0.013368,0.032916&vpsrc=0&ie=UTF8&hq=&hnear=855+Spruance+Ln,+Foster+City,+California+94404&t=h&z=17&iwloc=A";
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(mapUrl));
+        startActivity(intent);
+      }
+    });
+    
+    getDirectionsButton.setOnClickListener(new OnClickListener() {
+      
+      @Override
+      public void onClick(View v) {
+        String mapUrl = "http://maps.google.com/maps?f=d&source=s_d&saddr=615+South+Idaho+Street,+San+Mateo,+CA&daddr=855+Spruance+Ln,+Foster+City,+CA+94404";
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(mapUrl));
+        startActivity(intent);
+      }
+    });
+    
+    contactAgentButton.setOnClickListener(new OnClickListener() {
+      
+      @Override
+      public void onClick(View v) {
+        String url = "tel:6175992159";
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+    
+    agentDetailsButton.setOnClickListener(new OnClickListener() {
+      
+      @Override
+      public void onClick(View v) {
+        String url = "http://tamichiu.com/";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+      }
+    });
+    
+    photosButton.setOnClickListener(new OnClickListener() {
+      
+      @Override
+      public void onClick(View v) {
+        Intent listIntent = new Intent(v.getContext(), PhotoGallery.class);
+        v.getContext().startActivity(listIntent);
+      }
+    });
+    
   }
 
 }
---
+++ android/InfoIt/src/com/infoit/main/PhotoGallery.java
@@ -0,0 +1,123 @@
+package com.infoit.main;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.infoit.main.R.layout;
+
+public class PhotoGallery extends Activity {
+  private final GestureDetector gdt = new GestureDetector(new GestureListener());
+
+  private static final int SWIPE_MIN_DISTANCE = 120;
+  private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+  private ArrayList<Drawable> images;
+  private int position;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(layout.photo_gallery);
+
+    position = 0;
+
+    setupTestImages();
+    setDisplayImage();
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+
+    setContentView(R.layout.photo_gallery);
+
+    setDisplayImage();
+  }
+
+  private void setDisplayImage() {
+    ImageView displayImage = (ImageView) findViewById(R.id.photo);
+    displayImage.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(final View view, final MotionEvent event) {
+        gdt.onTouchEvent(event);
+        return true;
+      }
+    });
+    displayImage.setImageDrawable(images.get(position));
+  }
+
+  private void setupTestImages() {
+    images = new ArrayList<Drawable>();
+    images.add(getResources().getDrawable(R.drawable.house_1));
+    images.add(getResources().getDrawable(R.drawable.house_2));
+    images.add(getResources().getDrawable(R.drawable.house_3));
+    images.add(getResources().getDrawable(R.drawable.house_4));
+  }
+
+  private class GestureListener extends SimpleOnGestureListener {
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+        float velocityY) {
+      ImageView displayImage = (ImageView) findViewById(R.id.photo);
+
+      if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
+          && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+        if (position < images.size() - 1) {
+          position = position + 1;
+          displayImage.setImageDrawable(images.get(position));
+        }
+        return false; // Right to left
+      } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
+          && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+        if (position > 0) {
+          position = position - 1;
+          displayImage.setImageDrawable(images.get(position));
+        }
+        return false; // Left to right
+      }
+      if (e1.getY() - e2.getY() > SWIPE_MIN_DISTANCE
+          && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+        return false; // Bottom to top
+      } else if (e2.getY() - e1.getY() > SWIPE_MIN_DISTANCE
+          && Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {
+        return false; // Top to bottom
+      }
+      return false;
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+      LinearLayout backToInfoButton = (LinearLayout) findViewById(R.id.back_to_info_button);
+
+      if (View.GONE == (backToInfoButton.getVisibility())) {
+        backToInfoButton.setVisibility(View.VISIBLE);
+        backToInfoButton.setOnClickListener(new OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent listIntent = new Intent(v.getContext(),
+                DisplayEntityInfo.class);
+            v.getContext().startActivity(listIntent);
+          }
+        });
+      } else {
+        backToInfoButton.setVisibility(View.GONE);
+      }
+
+      return super.onSingleTapConfirmed(e);
+    }
+  }
+}

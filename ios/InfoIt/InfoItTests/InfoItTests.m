48e4f39130812a38da8a218230d1ad8bc2aee672
Christian Deonier <cdeonier@gmail.com>
2012-03-11 19:25:01 -0700

    Adding ios project

---
+++ ios/InfoIt/InfoItTests/InfoItTests.m
@@ -0,0 +1,32 @@
+//
+//  InfoItTests.m
+//  InfoItTests
+//
+//  Created by Christian Deonier on 3/10/12.
+//  Copyright (c) 2012 MIT. All rights reserved.
+//
+
+#import "InfoItTests.h"
+
+@implementation InfoItTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in InfoItTests");
+}
+
+@end

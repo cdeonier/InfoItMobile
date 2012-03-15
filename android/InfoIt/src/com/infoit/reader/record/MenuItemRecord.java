934787075179ea0a677c28a7a0d4b271d581ebb9
unknown <Christian@Theseus.(none)>
2012-03-14 23:16:01 -0700

    initial menu item

---
+++ android/InfoIt/src/com/infoit/reader/record/MenuItemRecord.java
@@ -0,0 +1,41 @@
+package com.infoit.reader.record;
+
+public class MenuItemRecord {
+	private String mName;
+	private String mDescription;
+	private String mPrice;
+	private String mThumbnailUrl;
+	
+	public MenuItemRecord() {
+		mName = "name";
+		mDescription = "description";
+		mPrice = "10";
+		mThumbnailUrl = "http://s3-us-west-1.amazonaws.com/infoit-photos/places/piperade_thumbnail_1.jpg";
+	}
+	
+	public String getName() {
+		return mName;
+	}
+	public void setName(String name) {
+		mName = name;
+	}
+	public String getDescription() {
+		return mDescription;
+	}
+	public void setDescription(String description) {
+		mDescription = description;
+	}
+	public String getPrice() {
+		return mPrice;
+	}
+	public void setPrice(String price) {
+		mPrice = price;
+	}
+	public String getThumbnailUrl() {
+		return mThumbnailUrl;
+	}
+	public void setThumbnailUrl(String thumbnailUrl) {
+		mThumbnailUrl = thumbnailUrl;
+	}
+
+}
